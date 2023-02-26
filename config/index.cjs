26edf0e78e9bc362c07350488e07e4b64558af2c
Feeling.Yan <Feeling.Y.an@qq.com>
2023-02-27 02:11:10 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,9 +21,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'opX1O5giApnJESTMCYFfxNfVPJrQ',
+      id: 'opX1O5h5Lj4Z2RHX1TuWVHrAZjrg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'akguNCVoZERMHYxxG8H0UBaTdxj1zcuOt7eqZc0433w',
+      useTemplateId: 'nTE3ObQP1IhU4S9oSNAAsWBSezEhmSlYtIQFgKCJvHY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-19',
       festivals: [
